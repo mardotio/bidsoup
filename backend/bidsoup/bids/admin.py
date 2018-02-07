cecdab3a56a91db2322c9473f7a1373b0ea4a698
Jayson Cleary <jaycle@users.noreply.github.com>
2018-02-06 22:27:02 -0500

    Add initial models for a bid (#5)
    This adds the models for the basic components of creating a bid
    object and creates the migrations. The models are also registered
    in the admin module for data testing.

--- backend/bidsoup/bids/admin.py
+++ backend/bidsoup/bids/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Customer, Category, ItemType, BidItem, BidTask, Bid
+
+admin.site.register(Bid)
+admin.site.register(BidItem)
+admin.site.register(ItemType)
+admin.site.register(Customer)
+admin.site.register(Category)
--- backend/bidsoup/bids/models.py
+++ backend/bidsoup/bids/models.py
@@ -1,3 +1,65 @@
 from django.db import models
 
-# Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField()
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    markup_percent = models.DecimalField(max_digits=6, decimal_places=3)
+    color = models.CharField(max_length=6)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+class ItemType(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    UNIT_CHOICES = (
+        ('EA', 'Each'),
+        ('PR', 'Pair'),
+        ('FT', 'Linear Feet'),
+        ('LFT', 'Linear Feet'),
+        ('FT2', 'Square Feet'),
+        ('FT3', 'Cubic Feet'),
+        ('HR', 'Hour'),
+        ('WK', 'Week'),
+    )
+    unit = models.CharField(max_length=3, choices=UNIT_CHOICES, default='EA')
+    unit_price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+class BidItem(models.Model):
+    bid = models.ForeignKey('Bid', on_delete=models.CASCADE, db_index=True)
+    item_type = models.ForeignKey(ItemType, on_delete=models.PROTECT)
+    notes = models.TextField(blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    markup_percent = models.DecimalField(max_digits=6, decimal_places=3)
+    quantity = models.DecimalField(max_digits=7, decimal_places=2)
+    parent = models.ForeignKey('BidTask', on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.item_type
+
+class BidTask(models.Model):
+    parent = models.ForeignKey('BidTask', on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+class Bid(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    bid_date = models.DateField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.name

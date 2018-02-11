0b9e5521997380fccf9c0a44d302f61a72c2cdee
Jayson Cleary <jayson.cleary@gmail.com>
2018-02-11 00:44:07 -0500

    Add fields to BidItem model
    The BidItem model is modified to include a price and a description.
    Additionally, the ItemType model is renamed to UnitType and the
    fixture is updated with the new names. The README is also updated
    with information for how to load and reset data.

--- backend/bidsoup/bids/admin.py
+++ backend/bidsoup/bids/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
-from .models import Customer, Category, ItemType, BidItem, BidTask, Bid, BidTask
+from .models import Customer, Category, UnitType, BidItem, BidTask, Bid, BidTask
 
 admin.site.register(Bid)
 admin.site.register(BidItem)
 admin.site.register(BidTask)
-admin.site.register(ItemType)
+admin.site.register(UnitType)
 admin.site.register(Customer)
 admin.site.register(Category)

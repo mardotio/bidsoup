161fb9401a399fc026368e9104feb6f4bfea5f75
Mario Lopez Robles <e.miliozx.er@gmail.com>
2018-02-18 16:59:18 -0500

    Connect current components to redux store

--- frontend/bidsoup/src/App.js
+++ frontend/bidsoup/src/App.js
@@ -1,52 +1,10 @@
 import React, { Component } from 'react';
-import Table from './jobitemtable/components/Table';
+import JobItem from './jobitemtable/components/JobItem';
 
-const data = {
-  category: 'labor',
-  color: '#05b57a',
-  columns: [{
-      name: 'description',
-      style: 'text'
-    },{
-      name: 'quantity',
-      style: 'number'
-    },{
-      name: 'price',
-      style: 'currency'
-    },{
-      name: 'total',
-      style: 'currency'
-    }
-  ],
-  rows: [{
-    description: 'This is a long description for an item',
-    quantity: 10,
-    price: 9999.99
-  },{
-    description: 'Some description',
-    quantity: 13,
-    price: 5400
-  },{
-    description: 'Some description',
-    quantity: 8,
-    price: 5141
-  },{
-    description: 'Some description',
-    quantity: 4,
-    price: 2540
-  },{
-    description: 'Some description',
-    quantity: 16,
-    price: 2354
-  }]
-}
-
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
-      <Table {...data} />
+      <JobItem />
     );
   }
 }
-
-export default App;
---
+++ frontend/bidsoup/src/jobitemtable/reducers/reducer_jobItems.js
@@ -0,0 +1,43 @@
+const JobItemsReducer = () => {
+  return [{
+    category: 'labor',
+    color: '#05b57a',
+    columns: [{
+        name: 'description',
+        style: 'text'
+      },{
+        name: 'quantity',
+        style: 'number'
+      },{
+        name: 'price',
+        style: 'currency'
+      },{
+        name: 'total',
+        style: 'currency'
+      }
+    ],
+    rows: [{
+      description: 'This is a long description for an item',
+      quantity: 10,
+      price: 9999.99
+    },{
+      description: 'Some description',
+      quantity: 13,
+      price: 5400
+    },{
+      description: 'Some description',
+      quantity: 8,
+      price: 5141
+    },{
+      description: 'Some description',
+      quantity: 4,
+      price: 2540
+    },{
+      description: 'Some description',
+      quantity: 16,
+      price: 2354
+    }]
+  }];
+};
+
+export default JobItemsReducer;

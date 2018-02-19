7e24f0c1b2b85421551c33873c4d9483bba81f16
Mario Lopez Robles <e.miliozx.er@gmail.com>
2018-02-18 22:37:41 -0500

    Add title to jobItem container

--- frontend/bidsoup/src/jobitemtable/components/Item.js
+++ frontend/bidsoup/src/jobitemtable/components/Item.js
@@ -2,26 +2,40 @@ import React from 'react';
 import styled from 'styled-components';
 import Table from './Table';
 
+const ItemWrapper = styled.div`
+  margin-top: 1em;
+`
+
+const JobItemHeader = styled.div`
+  margin-left: 1em;
+`
+
 const TableContainer = styled.div`
   display: flex;
+  margin-left: 1em;
 `
 
 const createCategoryTables = ({jobItems}) => {
-    let categoryTables = jobItems.map(item => {
-      return (
-        <Table 
-          key={item.category}
-          {...item}
-        />
-      );
-    });
-    return categoryTables;
+  let categoryTables = jobItems.map(item => {
+    return (
+      <Table 
+        key={item.category}
+        {...item}
+      />
+    );
+  });
+  return categoryTables;
 };
 
 const Item = props => (
-  <TableContainer>
-    {createCategoryTables(props)}
-  </TableContainer>
+  <ItemWrapper>
+    <JobItemHeader>
+      Add handrails to tank
+    </JobItemHeader>
+    <TableContainer>
+      {createCategoryTables(props)}
+    </TableContainer>
+  </ItemWrapper>
 );
 
 export default Item;

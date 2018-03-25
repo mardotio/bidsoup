2b8aaf0baad258eff4a103b7587fd03718e48b9b
Mario Lopez Robles <e.miliozx.er@gmail.com>
2018-03-25 15:30:56 -0400

    Add intro animation
    Added animation to FAB so that the button grows in when the page or view
    is first loaded. Also organized all css props in alphabetical oreder.

--- frontend/bidsoup/src/app/components/Fab.js
+++ frontend/bidsoup/src/app/components/Fab.js
@@ -1,50 +1,56 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import PropTypes from 'prop-types';
 import { determineFontColor } from '../utils/styling'
 
 const defaultSize = '56px';
 const miniSize = '40px';
 
+const expandIn = keyframes`
+  0%{transform: scale(0)}
+  100%{transform: scale(1)}
+`;
+
 const ButtonContainer = styled.div`
-  display: flex;
-  border-radius: 50%;
-  cursor: pointer;
-  margin: 24px;
-  height: ${({isMini}) => isMini ? miniSize : defaultSize};
-  width: ${({isMini}) => isMini ? miniSize : defaultSize};
-  background-color: ${({buttonColor}) => buttonColor};
-  color: ${({buttonColor, overrideFont}) => determineFontColor(
-    buttonColor, overrideFont, '#757575'
-  )};
   align-items: center;
-  justify-content: center;
-  user-select: none;
+  animation: ${expandIn} .28s ease-in;
+  background-color: ${({buttonColor}) => buttonColor};
+  border-radius: 50%;
   box-shadow:
     0 2px 2px 0 rgba(0, 0, 0, 0.14),
     0 1px 5px 0 rgba(0, 0, 0, 0.12),
     0 3px 1px -2px rgba(0, 0, 0, 0.2);
+  color: ${({buttonColor, overrideFont}) => determineFontColor(
+    buttonColor, overrideFont, '#757575'
+  )};
+  cursor: pointer;
+  display: flex;
+  height: ${({isMini}) => isMini ? miniSize : defaultSize};
+  justify-content: center;
+  margin: 24px;
   transition: box-shadow .28s;
+  user-select: none;
+  width: ${({isMini}) => isMini ? miniSize : defaultSize};
   &:hover {
     box-shadow:
       0 8px 10px 1px rgba(0, 0, 0, 0.14),
       0 3px 14px 2px rgba(0, 0, 0, 0.12),
       0 5px 5px -3px rgba(0, 0, 0, 0.2);
   }
-`
+`;
 
 const ButtonIcon = styled.i`
   height: 24px;
   width: 24px;
-`
+`;
 
 const Fab = props => {
   return (
     <ButtonContainer
       buttonColor={props.color}
-      overrideFont={props.overrideColor}
       isMini={props.mini}
       onClick={() => props.onClick()}
+      overrideFont={props.overrideColor}
     >
       <ButtonIcon className="material-icons">
         {props.icon}

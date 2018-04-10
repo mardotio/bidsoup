a980bf62fbbaf5b786746d666a937b1e2897f72a
Jayson Cleary <jayson.cleary@gmail.com>
2018-04-09 21:30:16 -0400

    Use url instead of id for resources
    Since URLs for REST are a unique way to describe a resource
    and are more informative than ids, this change exchanges the
    id field from resource serializers for a more descriptive url
    field. This also more closely matches what is returned for
    related fields.

--- backend/bidsoup/bids/serializers.py
+++ backend/bidsoup/bids/serializers.py
@@ -6,36 +6,31 @@ from rest_framework_recursive.fields import RecursiveField
 class BidSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Bid
-        fields = ('id', 'name', 'description', 'bid_date', 'customer', 'tax_percent')
+        fields = ('url', 'name', 'description', 'bid_date', 'customer', 'tax_percent')
 
 class BidItemSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = BidItem
-        fields = ('id', 'bid', 'unit_type', 'price', 'description', 'notes', 'category', 'markup_percent', 'quantity', 'parent')
+        fields = ('url', 'bid', 'unit_type', 'price', 'description', 'notes', 'category', 'markup_percent', 'quantity', 'parent')
 
 class BidTaskSerializer(serializers.HyperlinkedModelSerializer):
     children = serializers.ListField(source='children.all', read_only=True, child=RecursiveField())
 
     class Meta:
         model = BidTask
-        fields = ('id', 'parent', 'children', 'bid', 'title', 'description')
+        fields = ('url', 'parent', 'children', 'bid', 'title', 'description')
 
 class CategorySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Category
-        fields = ('id', 'bid', 'name', 'description', 'markup_percent', 'color')
+        fields = ('url', 'bid', 'name', 'description', 'markup_percent', 'color')
 
 class CustomerSerializer(serializers.HyperlinkedModelSerializer):
-    class BidSer(serializers.HyperlinkedModelSerializer):
-        class Meta:
-            model = Bid
-            fields = ('id', 'name', 'bid_date')
-
     class Meta:
         model = Customer
-        fields = ('id', 'name', 'email', 'phone')
+        fields = ('url', 'name', 'email', 'phone')
 
 class UnitTypeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = UnitType
-        fields = ('id', 'name', 'description', 'unit', 'unit_price')
+        fields = ('url', 'name', 'description', 'unit', 'unit_price')

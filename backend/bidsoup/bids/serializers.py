e962c4bbe033df1604b0fdc93766e1f53d07c8ea
Jayson Cleary <jayson.cleary@gmail.com>
2018-06-01 21:23:39 -0400

    Add links to related resources bid detail view
    When requesting a specific bid resource, serialize the links to the
    bidtask, biditem and category resources for that bid.

--- backend/bidsoup/bids/serializers.py
+++ backend/bidsoup/bids/serializers.py
@@ -1,12 +1,39 @@
 from .models import Bid, BidTask, BidItem, Category, Customer, UnitType
 from rest_framework import serializers
 from rest_framework_recursive.fields import RecursiveField
+from rest_framework_nested.relations import NestedHyperlinkedRelatedField
 
 
 class BidSerializer(serializers.HyperlinkedModelSerializer):
+    def __init__(self, *args, **kwargs):
+        # Don't pass custom kwargs to super
+        fields = kwargs.pop('fields', None)
+        exclude_fields = kwargs.pop('exclude_fields', None)
+
+        assert not (fields and exclude_fields), 'Cannot provide fields and exclude_fields'
+
+        super().__init__(*args, **kwargs)
+
+        if fields is not None:
+            # Drop all fields not specified
+            allowed = set(fields)
+            existing = set(self.fields)
+            for field_name in existing - allowed:
+                self.fields.pop(field_name)
+        elif exclude_fields is not None:
+            # Remove all fields listed
+            for field_name in exclude_fields:
+                self.fields.pop(field_name)
+
+
+    biditems = serializers.HyperlinkedIdentityField(view_name='bid-biditem-list', lookup_url_kwarg='bid_pk', many=False)
+    bidtasks = serializers.HyperlinkedIdentityField(view_name='bid-bidtask-list', lookup_url_kwarg='bid_pk', many=False)
+    categories = serializers.HyperlinkedIdentityField(view_name='bid-category-list', lookup_url_kwarg='bid_pk', many=False)
+
     class Meta:
         model = Bid
-        fields = ('url', 'name', 'description', 'bid_date', 'customer', 'tax_percent')
+        fields = ('url', 'name', 'description', 'bid_date', 'customer', 'tax_percent', 'biditems', 'bidtasks', 'categories')
+
 
 class BidItemSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:

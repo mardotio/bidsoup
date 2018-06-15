e7111b0a5905ca769651401432221e6cbbcd0cb4
Mario Lopez Robles <e.miliozx.er@gmail.com>
2018-06-14 22:09:56 -0400

    Recursively find nested objects

--- frontend/bidsoup/src/app/taskItem/containers/JobItem.js
+++ frontend/bidsoup/src/app/taskItem/containers/JobItem.js
@@ -1,14 +1,13 @@
 import { connect } from 'react-redux';
 import Item from '../components/Item';
+import { nestedFind } from '../../utils/utils';
 
-const mapStateToProps = state => {
-  return {
-    tableData: state.bidData.categoryTablesData,
-    selectedTask: state.bidData.tasks.flattenedList.find(task =>(
-      task.url === state.bidData.selectedTask
-    ))
-  };
-};
+const mapStateToProps = ({bidData}) => (
+  {
+    tableData: bidData.categoryTablesData,
+    selectedTask: nestedFind(bidData.tasks.list, 'url', bidData.selectedTask, 'children')
+  }
+);
 
 const JobItem = connect(mapStateToProps)(Item);
 
--- frontend/bidsoup/src/app/utils/utils.ts
+++ frontend/bidsoup/src/app/utils/utils.ts
@@ -2,3 +2,17 @@
 export const isEmpty = (value: string | any[]) => (
   value.length === 0
 );
+
+// tslint:disable-next-line:no-any
+export const nestedFind = (arr: Object[], key: string, match: any, nestedKey: string) => {
+  let result: Object | null = null;
+  arr.find(el => {
+    if (el[key] === match) {
+      result = el;
+    } else if (!isEmpty(el[nestedKey])) {
+      result = nestedFind(el[nestedKey], key, match, nestedKey);
+    }
+    return result != null;
+  });
+  return result;
+};

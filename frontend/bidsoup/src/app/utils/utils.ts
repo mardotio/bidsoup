5960fe9135a04b3a252f2783e4070ddd7413f248
Mario Lopez <e.miliozx.er@gmail.com>
2018-06-24 16:20:29 -0400

    Use custom type for nested search

--- frontend/bidsoup/src/app/utils/utils.ts
+++ frontend/bidsoup/src/app/utils/utils.ts
@@ -3,8 +3,13 @@ export const isEmpty = (value: string | any[]) => (
   value.length === 0
 );
 
-// tslint:disable-next-line:no-any
-export const nestedFind = (arr: Object[], key: string, match: any, nestedKey: string) => {
+type Nested<T, P extends keyof T> = {
+  [key in P]: T[]
+};
+
+export const nestedFind = 
+    <P extends keyof T, T extends Nested<T, P>, K extends keyof T>
+    (arr: T[], key: K, match: T[K], nestedKey: P) => {
   let result: Object | null = null;
   arr.find(el => {
     if (el[key] === match) {

3beeaeeaae9a102e0d7034842dd825f40d6a01d3
Jayson Cleary <jayson.cleary@gmail.com>
2018-07-05 22:31:51 -0400

    Simplify to remove return statements

--- frontend/bidsoup/src/app/utils/sorting.ts
+++ frontend/bidsoup/src/app/utils/sorting.ts
@@ -2,16 +2,12 @@ type HasString<K extends string> = {
   [key in K]: string
 };
 
-export const array2HashByKey = <T extends HasString<K>, K extends keyof T & string>(arr: T[], key: K) => {
-  return arr.reduce(
-    (ordered, el) => {
-      if (el[key] in ordered) {
-        ordered[el[key]].push(el);
-      } else {
-        ordered[el[key]] = [el];
-      }
-    return ordered;
-    },
-    {} as {[key in string]: T[]}
-  );
-};
+export const array2HashByKey = <T extends HasString<K>, K extends keyof T & string>(arr: T[], key: K) => (
+  arr.reduce(
+    (hash, el) => (
+      el[key] in hash
+        ? Object.assign(hash, {[el[key]]: [el, ...hash[el[key]]]})
+        : Object.assign(hash, {[el[key]]: el})
+    ),
+    {} as {[k in string]: T[]}
+  ));

743144b33676bba2dfed79d97d2468798d9b6fc9
Jayson Cleary <jayson.cleary@gmail.com>
2018-07-07 09:24:21 -0400

    Simplify by using Record builtin type

--- frontend/bidsoup/src/app/utils/sorting.ts
+++ frontend/bidsoup/src/app/utils/sorting.ts
@@ -1,13 +1,9 @@
-type HasString<K extends string> = {
-  [key in K]: string
-};
-
-export const array2HashByKey = <T extends HasString<K>, K extends keyof T & string>(arr: T[], key: K) => (
+export const array2HashByKey = <T extends Record<K, string>, K extends keyof T>(arr: T[], key: K) => (
   arr.reduce(
     (hash, el) => (
       el[key] in hash
         ? Object.assign(hash, {[el[key]]: [el, ...hash[el[key]]]})
         : Object.assign(hash, {[el[key]]: el})
     ),
-    {} as {[k in string]: T[]}
+    {} as {[k in T[K]]: T[]}
   ));

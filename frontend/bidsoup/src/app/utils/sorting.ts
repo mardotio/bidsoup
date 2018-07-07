9dfa59f1da28b657f8c695c0b1da54e9bd3cff0b
Jayson Cleary <jaycle@users.noreply.github.com>
2018-07-07 09:29:29 -0400

    Add types to array2HashByKey (#37)
    Add typescript types to array2HashByKey utility function. Since spread across
    generics is not supported, this also refactors a bit to use Object.assign in the
    reduce function.

--- frontend/bidsoup/src/app/utils/sorting.ts
+++ frontend/bidsoup/src/app/utils/sorting.ts
@@ -1,12 +1,9 @@
-// tslint:disable-next-line:no-any
-export const array2HashByKey = (arr: any[], key: any) => {
-  return arr.reduce(
-    (ordered, el) => ({
-      ...ordered,
-      [el[key]]: ordered[el[key]]
-        ? [...ordered[el[key]], el]
-        : [el]
-    }),
-    {}
-  );
-};
+export const array2HashByKey = <T extends Record<K, string>, K extends keyof T>(arr: T[], key: K) => (
+  arr.reduce(
+    (hash, el) => (
+      el[key] in hash
+        ? Object.assign(hash, {[el[key]]: [el, ...hash[el[key]]]})
+        : Object.assign(hash, {[el[key]]: el})
+    ),
+    {} as {[k in T[K]]: T[]}
+  ));

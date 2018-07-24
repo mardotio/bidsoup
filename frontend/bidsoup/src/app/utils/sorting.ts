14919d07d0d77b6930e683659c77d1354726aac5
Mario Lopez Robles <e.miliozx.er@gmail.com>
2018-07-24 19:45:50 -0400

    Fix bug in array2Hash function
    Each element of the hash returned by array2hash should be an array, but
    the current implementation did not always do this. array2hash would
    sometimes return an object as the value of an element if only a single
    object from the original array matched the value for the specified key.

--- frontend/bidsoup/src/app/utils/sorting.ts
+++ frontend/bidsoup/src/app/utils/sorting.ts
@@ -2,8 +2,8 @@ export const array2HashByKey = <T extends Record<K, string>, K extends keyof T>(
   arr.reduce(
     (hash, el) => (
       el[key] in hash
-        ? Object.assign(hash, {[el[key]]: [el, ...hash[el[key]]]})
-        : Object.assign(hash, {[el[key]]: el})
+        ? Object.assign(hash, {[el[key]]: [...hash[el[key]], el]})
+        : Object.assign(hash, {[el[key]]: [el]})
     ),
     {} as {[k in T[K]]: T[]}
   ));

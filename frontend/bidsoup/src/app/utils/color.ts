0ba6206b7648469ecccef4da1172503649e65be3
Mario Lopez Robles <e.miliozx.er@gmail.com>
2018-08-28 22:57:55 -0400

    Manage all color schemes in central location

--- frontend/bidsoup/src/app/utils/color.ts
+++ frontend/bidsoup/src/app/utils/color.ts
@@ -1,5 +1,37 @@
+// Returns hex color code based on concentartion of black
+// Expects a number from 0-100 (percent of black)
+export const shade = (percent: number) => (
+  `#${Math.round((100 - percent) * 255 / 100).toString(16).repeat(3)}`
+);
+
 export const textColor = {
-  active: '#212121',
-  inactive: '#666666',
-  disabled: '#9e9e9e'
+  dark: shade(87),
+  medium: shade(60),
+  light: shade(38)
+};
+
+export const theme = {
+  background: '#f3f6f9',
+  primary: '#4a2aea',
+  secondary: '#842aea',
+  accent: '#1180f7',
+  error: '#ff1744'
+};
+
+export const interactions = {
+  hover: shade(8)
+};
+
+export const components = {
+  border: shade(8),
+  scrollbar: shade(13),
+  darkBorder: shade(42)
+};
+
+export const categoryColors = {
+  color_1: `#b2135d`,
+  color_2: `#704bc6`,
+  color_3: `#24af63`,
+  color_4: `#ce5056`,
+  color_5: `#516077`
 };

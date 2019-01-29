94ac029a68786eae8620a73b5531222af92d02a6
Mario Lopez Robles <25378324+mardotio@users.noreply.github.com>
2019-01-28 20:21:12 -0500

    Allow alignment option

--- frontend/bidsoup/src/app/components/Grid.tsx
+++ frontend/bidsoup/src/app/components/Grid.tsx
@@ -8,6 +8,7 @@ interface Props {
   cells: JSX.Element[];
   containerId: string;
   maxColumns: number;
+  alignment?: 'left' | 'right' | 'center';
 }
 
 interface State {
@@ -17,18 +18,22 @@ interface State {
 
 interface ContainerProps {
   calculatedWidth: number;
-  sidePadding: number;
+  sidePadding: {
+    left: number;
+    right: number;
+  };
 }
 
 interface CellProps {
   cardWidth: number;
+  alignment: Props['alignment'];
 }
 
 const Container = styled.ul<ContainerProps>`
   width: ${props => props.calculatedWidth + 'px'};
   height: 100%;
-  padding-left: ${props => (props.sidePadding + 'px')};
-  padding-right: ${props => (props.sidePadding + 'px')};
+  padding-left: ${props => (props.sidePadding.left + 'px')};
+  padding-right: ${props => (props.sidePadding.right + 'px')};
   margin: 0;
   overflow: scroll;
   list-style: none;
@@ -43,10 +48,21 @@ const Container = styled.ul<ContainerProps>`
 const CellContainer = styled.li<CellProps>`
   width: ${props => props.cardWidth + 'em'};
   display: inline-block;
-  margin: 0 .5em 1em .5em
+  margin: ${props => {
+    switch (props.alignment) {
+      case 'left': return '0 1em 1em 0';
+      case 'center': return '0 .5em 1em .5em';
+      case 'right': return '0 0 1em 1em';
+      default: return '0';
+    }
+  }};
 `;
 
 export default class Grid extends React.Component<Props, State> {
+  static defaultProps = {
+    alignment: 'center'
+  };
+
   constructor(props: Props) {
     super(props);
     this.state = {
@@ -89,6 +105,7 @@ export default class Grid extends React.Component<Props, State> {
       <CellContainer
         key={key}
         cardWidth={cardWidth}
+        alignment={this.props.alignment}
       >
         {item}
       </CellContainer>
@@ -107,13 +124,35 @@ export default class Grid extends React.Component<Props, State> {
     return width + 5;
   }
 
+  calculatePadding = () => {
+    let width = this.calculateWidth();
+    switch (this.props.alignment) {
+      case 'left':
+        return {
+          left: 0,
+          right: (this.state.gridWidth - width)
+        };
+      case 'center':
+        return {
+          left: (this.state.gridWidth - width) / 2,
+          right: (this.state.gridWidth - width) / 2
+        };
+      case 'left':
+        return {
+          left: (this.state.gridWidth - width),
+          right: 0
+        };
+      default:
+        throw(`Grid does not support ${this.props.alignment} alignment`);
+    }
+  }
+
   cellArray = () => {
     let width = this.calculateWidth();
-    let padding = (this.state.gridWidth - width) / 2;
     return (
       <Container
         calculatedWidth={width}
-        sidePadding={padding}
+        sidePadding={this.calculatePadding()}
       >
         {this.props.cells.map(this.createCell)}
       </Container>

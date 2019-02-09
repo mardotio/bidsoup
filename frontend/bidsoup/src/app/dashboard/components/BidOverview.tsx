e05e070aa0fc4ee293207ab7c3b0f5807a32b66f
Mario Lopez Robles <25378324+mardotio@users.noreply.github.com>
2019-02-09 09:46:09 -0500

    Remove useless crew element

--- frontend/bidsoup/src/app/dashboard/components/BidOverview.tsx
+++ frontend/bidsoup/src/app/dashboard/components/BidOverview.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import styled from 'styled-components';
 import OverviewHeader from '@dashboard/components/OverviewHeader';
 import CategoryDashboard from '@dashboard/components/CategoryDashboard';
-import CrewDashboard from '@dashboard/components/CrewDashboard';
 import UnitDashboard from '@dashboard/components/UnitDashboard';
 import { CategoryWithItems } from '@dashboard/components/Dashboard';
 import { Actions } from '@taskItem/actions/unitTypeActions';
@@ -42,39 +41,6 @@ const bidTotal = ({categoriesWithItems}: Props) => (
 );
 
 const BidOverview = (props: Props) => {
-  const crew = [
-    {
-      first: 'John',
-      last: 'Doe',
-      position: 'Welder',
-      rate: 40,
-      color: '#42f4a1'
-    }, {
-      first: 'Bob',
-      last: 'Smith',
-      position: 'Cleaner',
-      rate: 50,
-      color: '#ff63b8'
-    }, {
-      first: 'Jason',
-      last: 'Brown',
-      position: 'Welder',
-      rate: 40,
-      color: '#fa903f'
-    }, {
-      first: 'James',
-      last: 'Clark',
-      position: 'Welder',
-      rate: 40,
-      color: '#ae5cf7'
-    }, {
-      first: 'David',
-      last: 'Cole',
-      position: 'Cleaner',
-      rate: 50,
-      color: '#fcc834'
-    }
-  ];
   return (
     <Container>
       <BidTitle>
@@ -91,9 +57,6 @@ const BidOverview = (props: Props) => {
       <UnitDashboard
         units={props.units}
         createUnitType={props.createUnitType}
-      />
-      <CrewDashboard
-        crew={crew}
       />
       <CategoryDashboard
         key={props.bid.name}

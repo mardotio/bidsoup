c42d27ce065f0086b528ec6a78273a2a5e48fc8f
Jayson Cleary <jayson.cleary@gmail.com>
2019-05-19 15:31:11 -0400

    Remove url namespacing

--- backend/bidsoup/bidsoup/urls.py
+++ backend/bidsoup/bidsoup/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('bids.urls', namespace='bids')),
+    path('api/', include('bids.urls')),
 ]

672f9a28aacdec26d1a3ddaac114b4b5cfe3adb3
Jayson Cleary <jayson.cleary@gmail.com>
2019-05-27 00:45:48 -0400

    Upgrade frontend dependencies
    Upgrade the following dependencies:
     * styled-components
     * typescript
     * material-ui
     * react
     * react-dom
     * react-redux

--- frontend/bidsoup/src/app/App.tsx
+++ frontend/bidsoup/src/app/App.tsx
@@ -1,27 +1,25 @@
 import * as React from 'react';
 import { Component } from 'react';
 import ViewContainer from '@app/containers/ViewContainer';
-import { JssProvider } from 'react-jss';
 import { create } from 'jss';
 import { Router, Switch, Route } from 'react-router';
 import LoginContainer from './login/containers/LoginContainer';
 import createBrowserHistory from 'history/createBrowserHistory';
-import { createGenerateClassName, jssPreset } from '@material-ui/core/styles';
 import { EmailSent } from './login/components/EmailSent';
+import { jssPreset, StylesProvider } from '@material-ui/styles';
 
 export const history = createBrowserHistory();
-const generateClassName = createGenerateClassName();
 const jss = create({
   ...jssPreset(),
   // Custom insertion point so styled-components can inject. See:
-  // https://material-ui.com/customization/css-in-js/#css-injection-order
-  insertionPoint: document.getElementById('jss-insertion-point')!
+  // https://material-ui.com/styles/advanced/#insertionpoint
+  insertionPoint: document.getElementById('jss-insertion-point') as HTMLElement
 });
 
 export default class App extends Component {
   render() {
     return (
-      <JssProvider jss={jss} generateClassName={generateClassName}>
+      <StylesProvider jss={jss}>
         <Router history={history}>
           <Switch>
             <Route path="/login" component={LoginContainer}/>
@@ -29,7 +27,7 @@ export default class App extends Component {
             <Route path="/" component={ViewContainer}/>
           </Switch>
         </Router>
-      </JssProvider>
+      </StylesProvider>
     );
   }
 }

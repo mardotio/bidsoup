7763eb8e34fe346c05864a556b966a037d6e3658
Mario Lopez Robles <25378324+mardotio@users.noreply.github.com>
2019-06-24 20:58:22 -0400

    Form for editing items

---
+++ frontend/bidsoup/src/app/taskItem/containers/EditBidItemFormContainer.ts
@@ -0,0 +1,38 @@
+import { AppState, BidItem, Category, Unit } from '@app/types/types';
+import { Actions, updateBidItem } from '@taskItem/actions/bidItemsActions';
+import { ThunkDispatch } from 'redux-thunk';
+import { connect } from 'react-redux';
+import EditBidItemForm from '@taskItem/components/EditBidItemForm';
+
+interface OwnProps {
+  item: BidItem;
+  onSave?: () => void;
+  onCancel: () => void;
+};
+
+interface StateProps {
+  categories: Category[];
+  units: Unit[];
+}
+
+interface DispatchProps {
+  updateBidItem: (i: BidItem) => Promise<Actions | void>;
+}
+
+const mapStateToProps = (state: AppState, ownProps: OwnProps): OwnProps & StateProps => ({
+  ...ownProps,
+  item: {
+    ...ownProps.item,
+    category: state.bidData.categories.list.find(c => c.name === ownProps.item.category)!.url,
+  },
+  categories: state.bidData.categories.list,
+  units: Object.keys(state.bidData.units.units).map(u => state.bidData.units.units[u])
+});
+
+const mapDispatchToProps = (dispatch: ThunkDispatch<AppState, never, Actions>): DispatchProps => ({
+  updateBidItem: (i: BidItem) => dispatch(updateBidItem(i))
+});
+
+const EditBidItemFormContainer = connect(mapStateToProps, mapDispatchToProps)(EditBidItemForm);
+
+export default EditBidItemFormContainer;

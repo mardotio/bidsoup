a1a93b032a85ff7a3e7615445cd0b55cb2968e4e
Mario Lopez Robles <25378324+mardotio@users.noreply.github.com>
2019-06-30 14:45:42 -0400

    Remove TSlint comment

--- frontend/bidsoup/src/app/utils/http.ts
+++ frontend/bidsoup/src/app/utils/http.ts
@@ -27,7 +27,6 @@ export class Http {
     return func(await response.json());
   }
 
-   // tslint:disable-next-line:no-any
   public static deleteJson = async <T, K>(uri: string, func: (url: string) => Option<T>) => {
     const response = await fetch(uri, {
       method: 'DELETE',

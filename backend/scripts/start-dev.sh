4d3fa1861662142e0f9ee19980df50127bacbda3
Jayson Cleary <jaycle@users.noreply.github.com>
2019-12-11 21:54:34 -0500

    Add cron job for clearing session data (#125)

--- backend/scripts/start-dev.sh
+++ backend/scripts/start-dev.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 #start-dev.sh - Script for starting development server.
 
+service cron start
+
 ./wait-for-it.sh -h db -p 5432 -- \
     echo "Database live. Starting app";\
     python /code/bidsoup/manage.py runserver --ptvsd 0.0.0.0:8000
--- backend/scripts/start-prod.sh
+++ backend/scripts/start-prod.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 #start-prod.sh - Script for starting production/test servers.
 
+service cron start
+
 ./wait-for-it.sh -h db -p 5432 -- \
     echo "Database live. Starting app";\
     python /code/bidsoup/manage.py migrate --noinput;\
